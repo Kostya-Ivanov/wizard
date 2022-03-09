6dd56c9a2b0d339abc4503f02d9818320da8b266
unknown <iv.const@mail.ru>
2022-03-09 22:35:30 +0300

    First commit

---
+++ src/Wizards.java
@@ -0,0 +1,21 @@
+public class Wizards {
+    String name;
+    public static void main(String[] args) {
+        Wizards[] w = new Wizards[3];
+        int z = -1;
+        while (z < 2) {
+            z = z + 1;
+            w[z] = new Wizards();
+            w[z].name = "Гарри Поттер";
+            if (z == 1) {
+                w[z].name = "Ронн";
+            }
+            if (z == 2) {
+                w[z].name = "Гермиона";
+            }
+            System.out.print(w[z].name + "-");
+            System.out.println("Имя настоящего волшебника");
+        }
+    }
+}
+
